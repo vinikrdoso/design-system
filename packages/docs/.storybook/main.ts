c3ac5a959821a8f264709d343c98ff9ddfbf525c
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-08-06 15:32:07 +0100

    fix: jekyll

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -32,7 +32,7 @@ const config: StorybookConfig = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/design-system/'
     }
 
     return config
